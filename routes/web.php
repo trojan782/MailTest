086e76d39d8100fc5b10ef681435272843e6323b
trojan782 <davidjacobs522@gmail.com>
2021-07-03 19:03:35 +0100

    added the mail class

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users');
 
