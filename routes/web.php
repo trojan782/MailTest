22c06018d07660f0f75e9ae9b80055de6f3aa0e1
trojan782 <davidjacobs522@gmail.com>
2021-07-03 21:35:25 +0100

    added the email route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Mail\NewUserNotification;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +25,8 @@ Auth::routes();
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users');
 
+//Route for mailing
+Route::get('/email', function () {
+    Mail::to('davidjacobs522@gmail.com')->send(new NewUserNotification());
+    return new NewUserNotification();
+});
