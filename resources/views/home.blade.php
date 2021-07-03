86e86a4ad06cd732d7605a25908a30d14d248d35
trojan782 <davidjacobs522@gmail.com>
2021-07-03 04:27:00 +0100

    reverted the dashboard to default

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="sm:container sm:mx-auto sm:mt-10">
-        <div class="w-full sm:px-6">
+<main class="sm:container sm:mx-auto sm:mt-10">
+    <div class="w-full sm:px-6">
 
-            @if (session('status'))
-                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4"
-                    role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
+        @if (session('status'))
+            <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
 
-            <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+        <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
-                <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-                    Dashboard
-                </header>
+            <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
+                Dashboard
+            </header>
 
-                <div class="w-full p-6">
-                    <p class="text-gray-700">
-                        <!-- component -->
-                    <div class="md:px-8 py-8 w-full">
-                        <div class="shadow overflow-hidden rounded border-b border-gray-200">
-                            <table class="min-w-full bg-white">
-                                <thead class="bg-purple-400 text-white">
-                                    <tr>
-                                        <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
-                                        <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Email Address</th>
-                                        <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Joined</td>
-                                    </tr>
-                                </thead>
-                                <tbody class="text-gray-700">
-                                    <tr>
-                                        <td class="w-1/3 text-left py-3 px-4">Trojan</td>
-                                        <td class="text-left py-3 px-4"><a class="hover:text-blue-500"
-                                                href="tel:622322662">trojan@localhost.com</a></td>
-                                        <td class="text-left py-3 px-4"><a class="hover:text-blue-500"
-                                                href="mailto:jonsmith@mail.com">1 hour ago</a></td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    </p>
-                </div>
-            </section>
-        </div>
-    </main>
+            <div class="w-full p-6">
+                <p class="text-gray-700">
+                    You are logged in!
+                </p>
+            </div>
+        </section>
+    </div>
+</main>
 @endsection
