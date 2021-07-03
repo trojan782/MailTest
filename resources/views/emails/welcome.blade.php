ed274ed258f396700419859aca8196d3f25282a9
trojan782 <davidjacobs522@gmail.com>
2021-07-03 21:36:36 +0100

    modified the email template

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-# Introduction
+# New User Alert ‼️
 
-The body of your message.
+A New user has just joined Use-ers
 
 @component('mail::button', ['url' => ''])
 Button Text
