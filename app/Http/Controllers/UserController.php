c73000666ac6242379bc32619dc8269cf61a62ff
trojan782 <davidjacobs522@gmail.com>
2021-07-03 04:21:00 +0100

    changed the users controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,22 +7,9 @@ use Illuminate\Foundation\Auth\User;
 
 class UserController extends Controller
 {
-    public function index(Request $request) {
-        $data = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'password' => 'required|string|min:4|confirmed'
-        ]);
-
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password'])
-        ]);
-
-        if($user) {
-            return view('home');
-        }
-        
+    public function index()
+    {
+        $users = User::latest()->paginate(7);
+        return view('users', ['users' => $users]);
     }
 }
