6db411c3b36d577d09dc706d02a4ab666fc52270
trojan782 <davidjacobs522@gmail.com>
2021-07-03 12:30:41 +0100

    changed the home route

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users');
 
