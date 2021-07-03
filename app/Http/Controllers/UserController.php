8f8b44a64973cec6dff0792acd048b3247052954
trojan782 <davidjacobs522@gmail.com>
2021-07-03 04:47:19 +0100

    added the middleware for the main page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Auth\User;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
     public function index()
     {
         $users = User::latest()->paginate(7);
